78523a653b9fa6bb0ef4e291538da01a7804caad
Thomas-Dautec <92532599+Thomas-Dautec@users.noreply.github.com>
2022-02-28 22:13:57 +0100

    Update Conversations.vue

--- src/views/Conversations.vue
+++ src/views/Conversations.vue
@@ -1,38 +1,33 @@
 <template>
-    <section class="section">
-        <p class='title'>Conversations</p>
-        <!--<router-link to-->
-        <div v-for='produit in $store.state.conversation'>
-            <p>{{produit}}</p>
-        </div>
-    </section>
+  <section>
+    <Navigation />
+    <div v-for="item in conversations" :key="item.id">
+      <Conversation class="m-3" :conversation="item" />
+    </div>
+  </section>
 </template>
 
 <script>
-export default{
-    data() {
-        return {
-            conversations : [],
-            nomConversation : ''
-        };
-    },
-    mounted(){/*
-        this.$api
-            .get('channels')
-            .then((response) => {
-            this.conversations = response.data;
-            console.log(response.data);
-        })*/
-        console.log("le composant App est affiché");
-        this.$api.get('channels').then(response => {
-            console.log(response.data);
-            let produits = response.data;
-            this.$store.commit('setConver', produits);
-        })
-    }
+import Conversation from '../components/Conversations.vue';
+export default {
+  components: { Conversation },
+  data(){
+    return {
+      conversations : []
+    };
+  },
+  mounted(){
+    this.$api
+    .get("channels")
+    .then((response) => {
+      this.conversations = response.data;
+    })
+    .catch((error) => {
+      alert(error.response.data.message);
+    });
+  },
 }
 </script>
 
 <style>
-
 </style>
