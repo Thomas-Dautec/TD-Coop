ff4af436a86b8d132facd61612c35db2f498e7ea
Thomas-Dautec <92532599+Thomas-Dautec@users.noreply.github.com>
2022-02-28 22:07:47 +0100

    ajout des conversations

---
+++ src/components/Message.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="card m-3">
+        <div class="card-content">
+            <div class="media">
+                <div class="media-content">
+                    <p class="title is-4">
+                        <router-link :to="'/membre/' + member.id">
+                            {{ member.fullname }}
+                        </router-link>
+                    </p>
+                    <p class="subtitle is-6">{{ member.email }}</p>
+                </div>
+            </div>
+            <div class="content" :data-id-message="message.id">
+               <p>{{ message.message }}</p>
+               <p>Envoyé le {{ message.created_at }}</p>
+            </div>
+        </div>
+        <footer class="card-footer">
+            <a class="card-footer-item" disabled>
+                <span class="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </span>
+                Modifier le message
+            </a>
+            <a @click="deleteMessage(message.id, member.id)" class="card-footer-item has-text-danger">
+                <span class="icon">
+                    <i class="fa-solid fa-trash"></i>
+                </span>
+                Supprimer le message
+            </a>
+        </footer>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+    props: ['message', 'deleteMessage'],
+    data() {
+        return {
+            member: false
+        }
+    },
+    computed: {
+        ...mapGetters(["getMember"]),
+    },
+    mounted() {
+        this.member = this.getMember(this.message.member_id);
+    },
+    
+}
+</script>
---
+++ src/components/posterMessage.vue
@@ -0,0 +1,45 @@
+<template>
+    <form class="box" @submit.prevent="poster">
+      <div class="field">
+          <div class="control">
+              <input type="text" class="input" v-model="message"/>
+          </div>
+      </div>
+      <div class="buttons">
+          <button class="button is-link"> Poster un message </button>
+      </div>
+    </form>
+</template>
+<script>
+export default {
+props : ['conversation'],
+ data(){
+        return {
+            message : '',
+        }
+    },
+    methods : {
+        poster(){
+            if (this.message == '') {
+                this.flashMessage.show({
+                    status: "error",
+                    title: "Message vide",
+                    message: "Il est impossible d'envoyer un message vide !",
+                    });
+            }
+            else{
+                this.$api.post(`channels/${this.conversation.id}/posts`,{
+                    channel_id : this.conversation.id,
+                    message : this.message,
+                    member_id : this.$store.state.member.id}).then(response =>{
+                        // console.log(response.data);
+                        this.$bus.$emit('charger-message', response.data)
+                    })
+                    this.message = ''
+            }
+            }
+        }
+}
+</script>
+
+<style>
